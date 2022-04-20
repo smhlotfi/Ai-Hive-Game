21da5aca3af0365168654764e667420e74a2f91a
smhlotfi <smhlotfizadeh@gmail.com>
2022-04-20 13:04:50 +0430

    Added "Move" and "ShowPossibility" function for Bee

--- Scripts/HexagonTypes/Bee.cs
+++ Scripts/HexagonTypes/Bee.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class Bee : Hexagon
 {
+    private static Bee _instance;
+    public static Bee Instance => _instance ??= FindObjectOfType<Bee>();
+    
+    public Dictionary<int, Hexagon> MovementPossibilities
+    {
+        get
+        {
+            return GameManager.Instance.movementPossibilities;
+        }
+        set
+        {
+            GameManager.Instance.movementPossibilities = value;
+        }
+    }
+    
     public void SelectBlackBee()
     {
         if (GameManager.Instance.turn == Turn.White) return;
@@ -14,5 +29,57 @@ public class Bee : Hexagon
     {
         if (GameManager.Instance.turn == Turn.Black) return;
         Select(HexagonColor.White, whiteSprite, Type.Bee);
+    }
+    
+    public void Move(Hexagon src, Hexagon dest, Sprite sprite, HexagonColor color, Type type)
+    {
+        src.GetComponent<SpriteRenderer>().sprite = idleSprite;
+        src._color = HexagonColor.NotDefined;
+        src._type = Type.Empty;
+
+        dest.GetComponent<SpriteRenderer>().sprite = sprite;
+        dest._color = color;
+        dest._type = type;
+
+        GameManager.Instance.RemoveFilledHexagon(src);
+        GameManager.Instance.AddFilledHexagon(dest);
+        HideMovementPossibilities();
+    }
+
+    public void ShowMovementPossibilities(Hexagon clickedHexagon)
+    {
+        var emptyAdjacent = clickedHexagon.GetEmptyAdjacent();
+        MovementPossibilities = new Dictionary<int, Hexagon>();
+        
+        foreach (var adj in emptyAdjacent)
+        {
+            var nonEmptyAdjs = adj.GetNonEmptyAdjacent();
+            if (nonEmptyAdjs.Count == 1 && nonEmptyAdjs.Contains(clickedHexagon)) continue;
+            MovementPossibilities.Add(adj.id, adj);
+        }
+        
+        foreach (var possibility in MovementPossibilities)
+        {
+            var hexagon = possibility.Value;
+            hexagon.isMovementPossibility = true;
+            var pos = hexagon.transform.position;
+            var rot = hexagon.transform.rotation;
+            var possibilityObject = Instantiate(possibilityPrefab, pos, rot);
+            GameManager.Instance.movementPossibilityPrefabs.Add(possibilityObject);
+        }
+    }
+    
+    public void HideMovementPossibilities()
+    {
+        if (MovementPossibilities.Count == 0) return;
+        foreach (var possibility in MovementPossibilities)
+        {
+            var hexagon = possibility.Value;
+            hexagon.isMovementPossibility = false;
+        }
+        foreach (var possibilityPrefab in GameManager.Instance.movementPossibilityPrefabs)
+        {
+            Destroy(possibilityPrefab);
+        }
     }
 }
